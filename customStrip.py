66aab296e8caa29af7ae00ec9b8419177f8061c3
braveadmin <adriobkork@gmail.com>
2021-12-20 17:04:08 +0100

    added customStrip.py exercise

---
+++ customStrip.py
@@ -0,0 +1,21 @@
+#! python3
+
+'''
+Write a function that takes a string and does the same thing as the strip() string method. 
+If no other arguments are passed other than the string to strip, then whitespace characters 
+will be removed from the beginning and end of the string. Otherwise, the characters specified 
+in the second argument to the function will be removed from the string.
+'''
+
+string = "       Automating the Boring Stuff with Python.    "
+
+def customStrip(string, noun=''):
+
+    if noun != '':    
+
+        string = string.replace(noun + ' ','')
+
+    print(string.strip())
+
+customStrip(string, 'Boring')
+
